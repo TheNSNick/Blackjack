a68ae161ed777301bb9210db44c1b7c9830c4276
Nicholas Kingzett <NSNick@gmail.com>
2017-06-16 11:42:12 -0400

    Started on v4 (final cleaned-up version?)

---
+++ v04/blackjack.py
@@ -0,0 +1,25 @@
+import dovetail
+import pygame
+from pygame.locals import *
+
+# CONSTANTS
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+CARD_WIDTH = 64
+CARD_HEIGHT = 88
+FPS = 60
+FONT_SIZE = 18
+LARGE_FONT_SIZE = 24
+HAND_OFFSET = 30
+SHOE_OFFSET = 3
+INTRO_COORDS = (, )
+DEALER_COORDS = (, )
+PLAYER_COORDS = (, )
+SHOE_COORDS = (, )
+RESULT_COORDS = (, )
+BG_COLOR = (0, 114, 0)
+FONT_COLOR = (255, 215, 0)
+FONT_NAME = 'timesnewroman'
+CHIP_FILE = 'chips.txt'
+CARD_BACK_IMAGE_FILE = 'card_back_crosshatch.png'
+CARD_SHEET_IMAGE_FILE = 'card_front_sheet.png'
